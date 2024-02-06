a75be73e81388ab40a8a7c55501e6b87551cff95
Domnicdev <142473218+Domnicdev@users.noreply.github.com>
2024-02-06 10:10:18 +0530

    Delete BackEnd directory

--- BackEnd/RentalMS/src/main/java/com/rms/repo/AddressRepo.java
+++
@@ -1,9 +0,0 @@
-package com.rms.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.rms.bean.Address;
-
-public interface AddressRepo extends CrudRepository<Address, Long>{
-
-}
--- BackEnd/RentalMS/src/main/java/com/rms/repo/HouseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.rms.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.rms.bean.House;
-
-public interface HouseRepo extends CrudRepository<House, Long> {
-
-}
