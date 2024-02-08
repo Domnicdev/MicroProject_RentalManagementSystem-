d3847160280fe318258dc3aeab6df352dba921c1
Domnicdev <142473218+Domnicdev@users.noreply.github.com>
2024-02-08 17:19:21 +0530

    Update PaymentRepo.java

--- BackEnd/src/main/java/com/rms/repo/PaymentRepo.java
+++ BackEnd/src/main/java/com/rms/repo/PaymentRepo.java
@@ -16,6 +16,5 @@ public interface PaymentRepo extends CrudRepository<Payment, Long> {
 	@Query(value = "select * from tbl_payment where due_amount>0", nativeQuery = true)
 	List<Payment> viewAllNonPaidTenants();
 
-	@Query(value = "select * from tbl_users where user_id in (select user_id from tbl_payment where due_amount > 0)", nativeQuery = true)
-	List<Payment> viewNonPaidTenantDetail();
+	
 }
